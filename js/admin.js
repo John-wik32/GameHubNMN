57e2746fe832fbf92ee73f4dd4e8eda820aedcca
John-wik32 <murtazaneejah12@gmail.com>
2025-12-15 17:56:16 -0500

    Refactor admin.js for improved functionality

--- js/admin.js
+++ js/admin.js
@@ -1,34 +1,62 @@
+const data = load();
 
-const data=load();
-
-document.getElementById('add').onclick=()=>{
- data.games.push({
-  id:uid(),
-  title:title.value,
-  logo:logo.value,
-  embed:embed.value,
-  tags:tags.value.split(','),
-  tab:tab.value,
-  likes:0,views:0,comments:[]
- });
- save(data);
- alert('Game added');
-};
-
-document.getElementById('saveTheme').onclick=()=>{
- data.theme.primary=primary.value;
- data.theme.bg=bg.value;
- save(data);
- alert('Theme saved');
-};
-
-document.getElementById('stats').innerHTML=
- 'Games: '+data.games.length+'<br>'+
- 'Views: '+data.games.reduce((a,b)=>a+b.views,0);
-
-function exportData(){
- const a=document.createElement('a');
- a.href=URL.createObjectURL(new Blob([JSON.stringify(data,null,2)]));
- a.download='games.json';
- a.click();
+function login() {
+  if (pass.value === data.password) {
+    login.style.display = "none";
+    panel.hidden = false;
+    renderAdmin();
+  } else alert("Wrong password");
 }
+
+function renderAdmin() {
+  stats.innerHTML =
+    "Games: " + data.games.length + "<br>" +
+    "Views: " + data.games.reduce((a,g)=>a+g.views,0);
+
+  tabList.innerHTML = "";
+  data.tabs.forEach((t,i)=>{
+    const li=document.createElement("li");
+    li.textContent=t;
+    li.draggable=true;
+    li.ondragstart=e=>e.dataTransfer.setData("i",i);
+    li.ondrop=e=>{
+      const from=e.dataTransfer.getData("i");
+      data.tabs.splice(i,0,data.tabs.splice(from,1)[0]);
+      save(data); renderAdmin();
+    };
+    li.ondragover=e=>e.preventDefault();
+    tabList.appendChild(li);
+  });
+}
+
+function addGame() {
+  data.games.push({
+    id: uid(),
+    title: title.value,
+    embed: embed.value,
+    tab: tab.value,
+    likes: 0,
+    views: 0,
+    comments: []
+  });
+  save(data);
+  alert("Game added");
+}
+
+function addTab() {
+  data.tabs.push(newTab.value);
+  save(data);
+  renderAdmin();
+}
+
+function saveTheme() {
+  data.theme = {
+    primary: primary.value,
+    bg: bg.value,
+    card: card.value,
+    text: text.value
+  };
+  save(data);
+  alert("Theme saved");
+}
+
